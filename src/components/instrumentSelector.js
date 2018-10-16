46c6fc84ef873e79a6b5df20e3a07c418d76fd3a
Paulo Barcelos <paulo@azucrina.org>
2018-10-16 18:42:39 +0200

    First day

---
+++ src/components/instrumentSelector.js
@@ -0,0 +1,111 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import ArrowLeft from 'src/assets/icons/general/arrowLeft.svg'
+import ArrowRight from 'src/assets/icons/general/arrowRight.svg'
+
+class InstrumentSelector extends React.Component {
+	onPrev = () => {
+		const {
+			instruments,
+			onSetInstrument
+		} = this.props
+		let {
+			instrument
+		} = this.props
+		instrument--
+		if (instrument < 0) {
+			instrument = instruments.length - 1
+		}
+		onSetInstrument(instrument)
+	}
+
+	onNext = () => {
+		const {
+			instruments,
+			onSetInstrument
+		} = this.props
+		let {
+			instrument
+		} = this.props
+		instrument++
+		if (instrument > instruments.length - 1) {
+			instrument = 0
+		}
+		onSetInstrument(instrument)
+	}
+
+	render() {
+		const {
+			instrument,
+			instruments
+		} = this.props
+
+		return (
+			<div className='root instrumentSelector'>
+				<style jsx>{`
+					.root {
+						display: flex;
+						flex-direction: row;
+						align-items: center;
+						justify-content: center;
+						margin-bottom: 5vh;
+					}
+					.arrow :global(svg) {
+						width: 8vh;
+						display: block;
+						cursor: pointer;
+					}
+					.name {
+						background-color: #939393;
+						color: #fff;
+						font-size: 5vh;
+						letter-spacing: 0.5vh;
+						text-transform: uppercase;
+						border-radius: 1vh;
+						height: 7vh;
+						display: flex;
+						flex-direction: row;
+						align-items: center;
+						justify-content: center;
+						width: 80vh;
+					}
+					@media (max-aspect-ratio: 15/10) {
+						.root {
+							margin-bottom: 5vw;
+						}
+						.arrow :global(svg) {
+							width: 8vw;
+						}
+						.name {
+							font-size: 5vw;
+							letter-spacing: 0.5vw;
+							border-radius: 1vw;
+							height: 7vw;
+							width: 80vw;
+						}
+					}
+				`}</style>
+				<div className='arrow'
+					onClick={this.onPrev}>
+					<ArrowLeft/>
+				</div>
+				<div className='name'>
+					{instruments && instruments[instrument].name}
+				</div>
+				<div className='arrow'
+					onClick={this.onNext}>
+					<ArrowRight/>
+				</div>
+			</div>
+		)
+	}
+}
+
+InstrumentSelector.propTypes = {
+	instruments   : PropTypes.arrayOf(PropTypes.shape({
+		name  : PropTypes.string,
+	})),
+	instrument : PropTypes.number,
+	onSetInstrument : PropTypes.func
+}
+export default InstrumentSelector
---
+++ src/components/key.js
@@ -0,0 +1,83 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+class Key extends React.Component {
+	onDown = () => {
+		const {
+			onKeyDown,
+			keyCode : key,
+		} = this.props
+		onKeyDown({ key })
+	}
+	onUp = () => {
+		const {
+			onKeyUp,
+			keyCode : key,
+		} = this.props
+		onKeyUp({ key })
+	}
+	render() {
+		const {
+			active,
+			color,
+			keyCode : key,
+		} = this.props
+
+		const events = {}
+		if ('ontouchstart' in document.documentElement) {
+			events.onTouchStart = this.onDown
+			events.onTouchEnd = this.onUp
+		} else {
+			events.onMouseDown = this.onDown
+			events.onMouseUp = this.onUp
+		}
+
+		return (
+			<div className={`root key ${active ? 'active' : ''}`}
+				{...events}>
+				<style jsx>{`
+					.root {
+						box-sizing: border-box;
+						background-color: #FFF;
+						border: solid 0.8vh ${color};
+						border-radius: 2vh;
+						margin: 1vh;
+						padding: 0 0 0 2.5vh;
+						font-size: 10vh;
+						color: ${color};
+						width: 20vh;
+						height: 20vh;
+						outline: none;
+						-webkit-tap-highlight-color: rgba(0,0,0,0);
+						text-transform: uppercase;
+						user-select: none;
+					}
+					.root.active {
+						background-color: ${color};
+						color: #fff;
+					}
+					@media (max-aspect-ratio: 15/10) {
+						.root {
+							border-width: 0.8vw;
+							border-radius: 1.5vw;
+							font-size: 7.5vw;
+							margin: 0.75vw;
+							width: 15vw;
+							height: 15vw;
+						}
+					}
+				`}</style>
+				{key}
+			</div>
+		)
+	}
+}
+
+Key.propTypes = {
+	keyCode   : PropTypes.string,
+	color     : PropTypes.string,
+	active    : PropTypes.bool,
+	onKeyDown : PropTypes.func,
+	onKeyUp   : PropTypes.func,
+}
+export default Key
